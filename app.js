452e505bb50952e613bb891d4f9c8da5560d5f3c
Connor Duggan <connor.duggan28@yahoo.com>
2023-01-21 14:33:10 -0500

    debug attempt

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-var map = L.map('map').setView([35.585, -80.8101], 13);
+const myMap = {
 
+}
+
+buildMap() ;{
+    this.map = L.map('map', {
+    center: this.coordinates,
+    zoom: 10,
+});
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+    attribution:
+     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(this.map)
+const marker = L.marker(this.coordinates)
+marker
+.addTo(this.map)
+.bindPopup('<p1><b>You are Here</b><br></p1>')
+.openPopup()
+}
+
+async function getCoords(){
+    const pos = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+    });
+    return [pos.coords.latitude, pos.coords.longitude]
+}
