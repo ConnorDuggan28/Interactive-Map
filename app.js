336bb440039f09e63fcad0db778bcd6d661373e0
Connor Duggan <connor.duggan28@yahoo.com>
2023-01-21 11:58:15 -0500

    got lost so had to start again

--- app.js
+++ app.js
@@ -1,17 +1,2 @@
-// start of map
-const myMap = {
-    
-}
-
-//build leaflet here
-buildMap() {
-    this.map = L.map('map', {
-        center: this.coordinates,
-        zoom: 11,
-    });
-}
- //link to openstreetmap
- L.tileLayer('https://.tile.openstreetmap.org///.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    minZoom: '15',
-}).addTo(this.map)
+var map = L.map('map').setView([35.5895, -80.8062], 14);
+L.tilelayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png')
